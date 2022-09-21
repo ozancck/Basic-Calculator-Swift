d4b2a7b6fa685e60a08efc6dbd6df03a82c2dff1
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-09-21 19:19:50 +0300

    added functions

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var num1: UITextField!
+    @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sum(_ sender: Any) {
+        if let firstNumber =  Double (num1.text!){
+            if let secondNumber = Double (num2.text!){
+                result = firstNumber + secondNumber
+                
+                resultLabel.text = String(result)
+            }else {resultLabel.text = "please enter number!"}
+        }else {resultLabel.text = "please enter number!"}
+    }
+    
+    
+    @IBAction func minus(_ sender: Any) {
+        if let firstNumber =  Double (num1.text!){
+            if let secondNumber = Double (num2.text!){
+                result = firstNumber - secondNumber
+                
+                resultLabel.text = String(result)
+            }else {resultLabel.text = "please enter number!"}
+        }else {resultLabel.text = "please enter number!"}
+    }
+    
+    
+    @IBAction func multiply(_ sender: Any) {
+        if let firstNumber =  Double (num1.text!){
+            if let secondNumber = Double (num2.text!){
+                result = firstNumber * secondNumber
+                
+                resultLabel.text = String(result)
+            }else {resultLabel.text = "please enter number!"}
+        }else {resultLabel.text = "please enter number!"}
+    }
+    
+    
+    @IBAction func divided(_ sender: Any) {
+        if let firstNumber =  Double (num1.text!){
+            if let secondNumber = Double (num2.text!){
+                result = firstNumber / secondNumber
+                
+                resultLabel.text = String(result)
+            }else {resultLabel.text = "please enter number!"}
+        }else {resultLabel.text = "please enter number!"}
+    }
+    
+    
 }
-
